71f327f52cef721de81ba49c454be48339d07ebe
moizusmancs <moizusmancs@gmail.com>
2025-08-25 23:51:42 +0500

    Time: 2 ms (64.68%) | Memory: 44.7 MB (80.01%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -1,24 +1,43 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
 
-        Map<Integer, Integer> map = new HashMap<>();
+        // Map<Integer, Integer> map = new HashMap<>();
 
-        for(Integer num: nums){
+        // for(Integer num: nums){
 
-            map.put(num, map.getOrDefault(num,0)+1);
+        //     map.put(num, map.getOrDefault(num,0)+1);
 
-        }
+        // }
 
-        int[] arr = new int[2];
-        int idx = 0;
-        for(Map.Entry<Integer, Integer> val: map.entrySet()){
-            if(val.getValue() > 1){
-                arr[idx] = val.getKey();
+        // int[] arr = new int[2];
+        // int idx = 0;
+        // for(Map.Entry<Integer, Integer> val: map.entrySet()){
+        //     if(val.getValue() > 1){
+        //         arr[idx] = val.getKey();
+        //         idx++;
+        //     }
+        // }
+
+        // return arr;
+
+        int arr[] = new int[2]; //1
+        Arrays.sort(nums); // nlogn
+        int idx = 0; //1
+
+        // n
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i] == nums[i+1]){
+                arr[idx] = nums[i];
                 idx++;
             }
         }
 
+        // nlogn
         return arr;
+
+
+
+
         
     }
 }
