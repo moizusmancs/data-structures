b34c5c1c7d654993cfbdef1c0019c026de4cc6e5
moizusmancs <moizusmancs@gmail.com>
2025-08-26 06:45:18 +0500

    Time: 7 ms (50.77%) | Memory: 56.7 MB (63.56%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,21 +1,43 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
 
-        Map<Integer, Integer> mp = new HashMap<>();
+        // Map<Integer, Integer> mp = new HashMap<>();
 
-        List<Integer> li = new ArrayList<>();
+        // List<Integer> li = new ArrayList<>();
 
-        for(Integer i : nums){
-            mp.put(i, mp.getOrDefault(i,0)+1);
-        }
+        // for(Integer i : nums){
+        //     mp.put(i, mp.getOrDefault(i,0)+1);
+        // }
 
-        for(int i=1; i<=nums.length; i++){
-            if(!(mp.containsKey(i))){
-                li.add(i);
+        // for(int i=1; i<=nums.length; i++){
+        //     if(!(mp.containsKey(i))){
+        //         li.add(i);
+        //     }
+        // }
+
+        // return li;
+
+        int n = nums.length;
+        int i = 0;
+
+        while(i<n){
+            int currIndex = nums[i]-1;
+            if(i != currIndex && nums[currIndex] != nums[i]){
+                int temp = nums[i];
+                nums[i] = nums[currIndex];
+                nums[currIndex] = temp;
+            }else{
+                i++;
             }
         }
 
-        return li;
+        List<Integer> li = new ArrayList<>();
+        for(int j=0; j<n; j++){
+            if(nums[j] != j+1){
+                li.add(j+1);
+            }
+        }
         
+        return li;
     }
 }
