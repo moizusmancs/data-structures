b84b1d61de1392e03a4e01dfa7d25ea599461dc0
Moiz Usman <moizusmancs@gmail.com>
2025-08-27 02:41:32 +0500

    Time: 0 ms (100.00%) | Memory: 41.3 MB (100.00%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+
+
+        int count = 0 ;
+
+        for (int hour : hours){
+            if(hour>=target){
+                count++;
+            }
+        }
+
+        return count;
+        
+    }
+}
