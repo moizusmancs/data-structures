39078ea12e3cf2a119d9f70df9af4a0754e6fd8e
Moiz Usman <moizusmancs@gmail.com>
2025-09-04 15:45:26 +0500

    Time: 0 ms (100.00%) | Memory: 41.1 MB (41.65%) - LeetSync

--- 3830-find-closest-person/find-closest-person.java
+++ 3830-find-closest-person/find-closest-person.java
@@ -1,12 +1,22 @@
 class Solution {
     public int findClosest(int x, int y, int z) {
-        if (Math.abs(x-z) == Math.abs(y-z)){
-            return 0;
-        }else if(Math.abs(x-z) < Math.abs(y-z)){
-            return 1;
-        }else{
-            return 2;
-        }
+        // if (Math.abs(x-z) == Math.abs(y-z)){
+        //     return 0;
+        // }else if(Math.abs(x-z) < Math.abs(y-z)){
+        //     return 1;
+        // }else{
+        //     return 2;
+        // }
+
+        // why above is a little inefficent, if absolute calculated, why not store it and not calculate again?
+
+        int xz = Math.abs(x-z);
+        int yz = Math.abs(y-z);
+
+        if (xz == yz) return 0;
+
+        if(xz<yz) return 1;
+        else return 2;
         
     }
 }
