b86c510f62cd701a5576aa99746f408a1dc68b59
Moiz Usman <moizusmancs@gmail.com>
2025-09-07 00:45:42 +0500

    Time: 1 ms (99.82%) | Memory: 53.5 MB (14.44%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        int count = 0;
+        int lastElement = nums[0];
+
+        for (int num:nums){
+            if(count == 0){
+                lastElement = num;
+            }
+            
+            if(num == lastElement){
+                count++;
+            }else{
+                count--;
+            }
+        }
+
+        return lastElement;
+        
+    }
+}
